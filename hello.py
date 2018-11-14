c0b1a0c438630298012a4d380c9a4af2e88dd8fe
Collins Mugarura <collismugy@hotmail.com>
2018-11-14 22:51:57 +0000

    added api to call c# api deployed in Azure

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from typing import Optional
+
+from flask import Flask, jsonify
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +12,19 @@ def hell0():
 
 @app.route('/employees')
 def get_employees():
-    pass
+    url: str='https://employeewebapiazure20180822105822.azurewebsites.net/api/employees'
+    response= requests.get(url)
+
+    if(response.ok):
+        content: Optional[bytes] = response.content
+        jsonData: object = json.loads(content)
+        data = jsonify({'employees' : jsonData})
+        return data
+
+    else:
+        response.raise_for_status()
+
+    
+
 
 app.run(port=5000)
